c4580bb60ec464909c99d91804f461e209226d8f
Yassafouad <yassayassa16@gmail.com>
2024-10-06 21:13:44 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import SubHeading from './SubHeading/SubHeading';
+import Newsletter from './Footer/Newsletter';
+import FooterOverlay from './Footer/FooterOverlay';
+import Navbar from './Navbar/Navbar';
+import MenuItem from './Menuitem/MenuItem';
+
+// يجب أن يكون هناك سطر فارغ هنا
+
+export {
+  SubHeading,
+  Newsletter,
+  FooterOverlay,
+  MenuItem,
+  Navbar,
+};
